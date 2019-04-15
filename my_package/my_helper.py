b532abf5731182722bb0f97750ba1ba621f3b23b
Yogesh56931 <38547590+Yogesh56931@users.noreply.github.com>
2019-04-15 14:45:03 +1000

    Create my_helper.py

---
+++ my_package/my_helper.py
@@ -0,0 +1,88 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+# weight and bais wrappers
+def weight_variable(name, shape):
+    """
+    Create a weight variable with appropriate initialization
+    :param name: weight name
+    :param shape: weight shape
+    :return: initialized weight variable
+    """
+    initer = tf.truncated_normal_initializer(stddev=0.01)
+    return tf.get_variable('W_' + name,
+                           dtype=tf.float32,
+                           shape=shape,
+                           initializer=initer)
+
+def bias_variable(name, shape):
+    """
+    Create a bias variable with appropriate initialization
+    :param name: bias variable name
+    :param shape: bias variable shape
+    :return: initialized bias variable
+    """
+    initial = tf.constant(0., shape=shape, dtype=tf.float32)
+    return tf.get_variable('b_' + name,
+                           dtype=tf.float32,
+                           initializer=initial)
+
+def fc_layer(x, num_units, name, use_relu=True):
+    """
+    Create a fully-connected layer
+    :param x: input from previous layer
+    :param num_units: number of hidden units in the fully-connected layer
+    :param name: layer name
+    :param use_relu: boolean to add ReLU non-linearity (or not)
+    :return: The output array
+    """
+    with tf.variable_scope(name):
+        in_dim = x.get_shape()[1]
+        W = weight_variable(name, shape=[in_dim, num_units])
+        tf.summary.histogram('W', W)
+        b = bias_variable(name, [num_units])
+        tf.summary.histogram('b', b)
+        layer = tf.matmul(x, W)
+        layer += b
+        if use_relu:
+            layer = tf.nn.relu(layer)
+        return layer
+def write_sprite_image(filename, images,img_h=28,img_w=28):
+    """
+        Create a sprite image consisting of sample images
+        :param filename: name of the file to save on disk
+        :param shape: tensor of flattened images
+    """
+
+    # Invert grayscale image
+    images = 1 - images
+
+    # Calculate number of plot
+    n_plots = int(np.ceil(np.sqrt(images.shape[0])))
+
+    # Make the background of sprite image
+    sprite_image = np.ones((img_h * n_plots, img_w * n_plots))
+
+    for i in range(n_plots):
+        for j in range(n_plots):
+            img_idx = i * n_plots + j
+            if img_idx < images.shape[0]:
+                img = images[img_idx]
+                sprite_image[i * img_h:(i + 1) * img_h,
+                j * img_w:(j + 1) * img_w] = img
+
+    plt.imsave(filename, sprite_image, cmap='gray')
+    print('Sprite image saved in {}'.format(filename))
+
+def write_metadata(filename, labels):
+    """
+            Create a metadata file image consisting of sample indices and labels
+            :param filename: name of the file to save on disk
+            :param shape: tensor of labels
+    """
+    with open(filename, 'w') as f:
+        f.write("Index\tLabel\n")
+        for index, label in enumerate(labels):
+            f.write("{}\t{}\n".format(index, label))
+
+    print('Metadata file saved in {}'.format(filename))
